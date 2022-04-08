6f9d23297da02f131a15612431fa36e0a45f128d
Renuka Prasad <renu.yarasu@gmail.com>
2022-04-08 21:54:47 +0530

    Switch Statement: Switch Statement Works

--- script.js
+++ script.js
@@ -1,2 +1,21 @@
 // JavaScript Syntax and Operators
 console.clear();
+
+function simpleSwitch() {
+    let productId = 2;
+    switch (productId) {
+        case 1:
+            console.log('Product 1');
+            break;
+
+        case 2:
+            console.log('Product 2');
+            break;
+
+        case 3:
+            console.log('Product 3');
+            break;
+        default:
+            console.log('No products available');
+    }
+}
