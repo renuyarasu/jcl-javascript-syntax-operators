e2fee5e583e04692896b9426d7540ecec766b18b
Renuka Prasad <renu.yarasu@gmail.com>
2022-04-08 22:22:05 +0530

    Switch Statement: Block-level Scope Issue with Switch Statements

--- script.js
+++ script.js
@@ -1,24 +1,29 @@
-// JavaScript Syntax and Operators
 console.clear();
+// JavaScript Syntax and Operators
 
-function strictComparison() {
-    
-    let productId = '2'; //string
-
+function blockScopeProblem() {
+    let productId = 2;
     switch (productId) {
-        case 1:
-            console.log('Product 1');
+        case 1: {
+            let message = 'Product 1';
+            console.log(message);
             break;
+        }
+        case 2: {
+            let message = 'Product 2';
+            console.log(message);
+            break;
+        }
 
-        case 2:
-            console.log('Product 2');
+        case 3: {
+            let message = 'Product 3';
+            console.log(message);
             break;
+        }
 
-        case 3:
-            console.log('Product 3');
-            break;
         default:
-            console.log('No products available');
+            let message = 'No products available'
+            console.log(message);
     }
 }
 
