0851479998e35faee67d2a5e6608987166b93192
Renuka Prasad <renu.yarasu@gmail.com>
2022-04-12 15:44:12 +0530

    for-in Loop

--- script.js
+++ script.js
@@ -1,2 +1,22 @@
 // JavaScript Syntax and Operators
-console.clear();
+'use strict';
+
+// Using a for-in Loop
+
+
+function forInLoop() {
+    let product = {
+        'productID': 262,
+        'name': 'The Psychology of Money',
+        'productNumber': 'Psychology-262',
+        'author': 'Morgan Housel',
+        'standardCost': 320,
+        'listPrice': 320,
+        calculateGrossProfit: function () {
+            return this.listPrice - this.standardCost;
+        }
+    };
+    for (const key in product) { // 'in' keyword grabs all property and method names from an object.
+        console.log("'" + key + "'=" + product[key]);
+    }
+}
