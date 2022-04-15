035edcea412116d1b58abc9a0f5405e80b71f931
Renuka Prasad <renu.yarasu@gmail.com>
2022-04-15 11:09:55 +0530

    Using Math and Comparison Operators - Plus Sign with Strings and Numbers

--- script.js
+++ script.js
@@ -2,49 +2,17 @@
 console.clear();
 
 // Math Operators
-function mathOperators() {
+function plusSign() {
     let price = 100;
+    let stringsValue = '100';
     let result = 0;
-    console.log('price = ' + price.toString());
 
-    // Addition
-    result = price + 100;
-    console.log('price + 100 = ' + result.toString());
+    console.log('price = ' + price.toString()); //price = 100
 
-    // Subtraction
-    result = price - 100;
-    console.log('price - 100 = ') + result.toString();
-
-    // Multiplication
-    result = price * 10;
-    console.log('price * 10 = ' + result.toString());
-
-    // Division
-    result = price / 10;
-    console.log('price / 10 = ' + result.toString());
-
-    // Exponentiation
-    result = price ** 2;
-    console.log('price ** 2 = ' + result.toString());
-
-    //Modules
-    result = price % 5;
-    console.log('price % 5 = ' + result.toString());
-
-    //Increment
-    result = price++;
-    console.log('result = price++ = ' + result.toString());
-    console.log('price = ' + price.toString());
-    result = ++price;
-    console.log('result = ++price = ' + result.toString());
-
-    //Decrement
-    result = price--;
-    console.log('result = price-- = ' + result.toString());
-    console.log('price = ' + price.toString());
-    result = --price;
-    console.log('result = --price = ' + result.toString());
-
+    result = price + stringsValue;
+    console.log('result (number + stringsValue) = ' + result.toString()); // result (number + stringsValue) = 100100
     
-
+    result = price + (+stringsValue);
+    console.log('result (number + (+stringsValue)) = ' + result.toString()); // result (number + (+stringsValue)) = 200
+    
 }
