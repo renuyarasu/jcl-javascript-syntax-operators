b39806043a4678e8d7a4cda85d42bdf8c21f0428
Renuka Prasad <renu.yarasu@gmail.com>
2022-04-15 11:04:25 +0530

    Using Math and Comparison Operators - Mathematical Operators

--- script.js
+++ script.js
@@ -1,2 +1,50 @@
 // JavaScript Syntax and Operators
 console.clear();
+
+// Math Operators
+function mathOperators() {
+    let price = 100;
+    let result = 0;
+    console.log('price = ' + price.toString());
+
+    // Addition
+    result = price + 100;
+    console.log('price + 100 = ' + result.toString());
+
+    // Subtraction
+    result = price - 100;
+    console.log('price - 100 = ') + result.toString();
+
+    // Multiplication
+    result = price * 10;
+    console.log('price * 10 = ' + result.toString());
+
+    // Division
+    result = price / 10;
+    console.log('price / 10 = ' + result.toString());
+
+    // Exponentiation
+    result = price ** 2;
+    console.log('price ** 2 = ' + result.toString());
+
+    //Modules
+    result = price % 5;
+    console.log('price % 5 = ' + result.toString());
+
+    //Increment
+    result = price++;
+    console.log('result = price++ = ' + result.toString());
+    console.log('price = ' + price.toString());
+    result = ++price;
+    console.log('result = ++price = ' + result.toString());
+
+    //Decrement
+    result = price--;
+    console.log('result = price-- = ' + result.toString());
+    console.log('price = ' + price.toString());
+    result = --price;
+    console.log('result = --price = ' + result.toString());
+
+    
+
+}
