218f32f3fe1d277c5406cadd0eb38b9dd6373210
Renuka Prasad <renu.yarasu@gmail.com>
2022-04-15 12:46:03 +0530

    Using Math and Comparison Operators - Comparison and Ternary Operators

--- script.js
+++ script.js
@@ -1,34 +1,58 @@
 // JavaScript Syntax and Operators
 console.clear();
 
-// Math Operators: Assignment Operators
-function assignmentOperators() {
+// Math Operators: Comparison and Ternary Operators
+function comparisonOperators() {
     let price = 100;
+    let result = 0;
+
     console.log('price = ' + price.toString());
+    console.log('');
 
-    // Addition
-    price += 100;
-    console.log('price += 100 = ' + price.toString());// price += 100 = 200
+    // Equal to
+    console.log('price == price = ' + (price == price).toString()); // true
 
-    // Subtraction
-    price -= 100;
-    console.log('price -= 100 = ' + price.toString());// price += 100 = 100
+    // Equal to
+    result = price.toString();
+    console.log('price(number) == result(string) = ' + (price == result).toString()); // true
 
-    // Multiplication
-    price *= 10;
-    console.log('price *= 10 = ' + price.toString()); // price *= 10 = 1000
+    // Equal to / Equal type
+    result = price;
+    console.log('price(number) === result(string) = ' + (price === result).toString()); // true
 
-    // Division
-    price /= 10;
-    console.log('price /= 10 = ' + price.toString()); // price /= 10 = 100
+    // Equal to / Equal type
+    result = price.toString();
+    console.log('price(number) === result(string) = ' + (price === result).toString()); // false
+    console.log('');
 
-    // Exponentiation
-    price **= 2;
-    console.log('price **= 2 = ' + price.toString()); // price **= 2 = 10000
 
-    //Modules
-    price %= 5;
-    console.log('price %= 5 = ' + price.toString()); // price %= 5 = 0
+    // Not Equal to
+    result = 100;
+    console.log('result = ' + result.toString());
+    console.log('price(number) != result(string) = ' + (price != result).toString()); // false
+
+    // Not Equal to / Not Equal type
+    result = price.toString();
+    console.log('price(number) !== result(string) = ' + (price !== result).toString()); // true
+    console.log('');
+
+    // Greater Then 
+    console.log('price > 10 = ' + (price > 10).toString()); // true
+
+    // Greater Then or equal to 
+    console.log('price >= 10 = ' + (price >= 10).toString()); // true
+    console.log("");
+
+    // Less Then 
+    console.log('price < 10 = ' + (price < 10).toString()); // false
+
+    // Less Then or equal to 
+    console.log('price <= 10 = ' + (price <= 10).toString()); // false
+    console.log("");
+
+    // Ternary Operator
+    result = price < 1500 ? 'less then 1500 ' : 'greater then 1500.';
+    console.log('Ternary Operator = ' + result);
 
 }
 
