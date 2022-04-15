31556a935aeed536a4c57fa8b3e5216e81cf3b15
Renuka Prasad <renu.yarasu@gmail.com>
2022-04-15 11:17:32 +0530

    Using Math and Comparison Operators - Assignment Operators

--- script.js
+++ script.js
@@ -1,18 +1,40 @@
 // JavaScript Syntax and Operators
 console.clear();
 
-// Math Operators
-function plusSign() {
+// Math Operators: Assignment Operators
+function assignmentOperators() {
     let price = 100;
-    let stringsValue = '100';
-    let result = 0;
+    console.log('price = ' + price.toString());
 
-    console.log('price = ' + price.toString()); //price = 100
+    // Addition
+    price += 100;
+    console.log('price += 100 = ' + price.toString());// price += 100 = 200
 
-    result = price + stringsValue;
-    console.log('result (number + stringsValue) = ' + result.toString()); // result (number + stringsValue) = 100100
-    
-    result = price + (+stringsValue);
-    console.log('result (number + (+stringsValue)) = ' + result.toString()); // result (number + (+stringsValue)) = 200
-    
+    // Subtraction
+    price -= 100;
+    console.log('price -= 100 = ' + price.toString());// price += 100 = 100
+
+    // Multiplication
+    price *= 10;
+    console.log('price *= 10 = ' + price.toString()); // price *= 10 = 1000
+
+    // Division
+    price /= 10;
+    console.log('price /= 10 = ' + price.toString()); // price /= 10 = 100
+
+    // Exponentiation
+    price **= 2;
+    console.log('price **= 2 = ' + price.toString()); // price **= 2 = 10000
+
+    //Modules
+    price %= 5;
+    console.log('price %= 5 = ' + price.toString()); // price %= 5 = 0
+
 }
+
+
+
+
+
+
+
