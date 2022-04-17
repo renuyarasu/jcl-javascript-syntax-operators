f3f7751ed73afcd5bfc450e53139c4a22d5f926a
Renuka Prasad <renu.yarasu@gmail.com>
2022-04-17 20:42:06 +0530

    Exception Handlinge - Throw a Custom Error Object

--- script.js
+++ script.js
@@ -1,46 +1,25 @@
 // JavaScript Syntax and Operators
 console.clear();
-// tryCatch
-// finallyCatch
-// finallySuccess
 
-// Try, Catch
-function tryCatch() {
+
+// Throw a Custom Error Object
+
+function throwError() {
+    try {
+        attemptDivision();
+    } catch (error) {
+        console.log(error.message + ' - Error type: ' + error.name);
+    }
+}
+function attemptDivision() {
     let result;
     try {
         result = x / 10;
     } catch (error) {
-        console.log(error.message); // JavaScript error object always has 'name' and 'message' properties.
+        // Always include at least a 'message' and 'name' properties.
+        throw {
+            'message': ' In the attemptDivision() method the following error occer: ' + error.message,
+            'name': 'CustomError'
+        };
     }
 }
-
-// Finally Statement with Catch Blocks
-function finallyCatch() {
-    let result;
-    try {
-        console.log('An error will occure!');
-        result = x / 10;
-        } catch (error) {
-        console.log(`In the "Catch" Block: ` + error.message);
-    } finally {
-        console.log(`In the 'Finally' block!`);
-    }
-}
-
-
-// Finally Statement with Catch Success
-
-function finallySuccess() {
-    let result;
-    let x = 100;
-    try {
-        console.log('An error will occure!');
-        result = x / 10;
-        console.log('This line will never run.');
-    } catch (error) {
-        console.log(`In the "Catch" Block: ` + error.message);
-    } finally {
-        console.log(`In the 'Finally' block!`);
-    }
-}
-
