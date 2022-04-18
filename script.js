6ad7e031f2f802ec4a584e74f86b5053bc0bc0e7
Renuka Prasad <renu.yarasu@gmail.com>
2022-04-18 21:23:18 +0530

    Exception Handlinge - Detect the Error Type

--- script.js
+++ script.js
@@ -1,25 +1,87 @@
 // JavaScript Syntax and Operators
 console.clear();
+'use strict'
+// Detect the Error Type
+// https://blog.bitsrc.io/types-of-native-errors-in-javascript-you-must-know-b8238d40e492
 
+// ReferenceError
+// RangeError
+// TypeError
+// URIError 
+// SyntaxError
+// EvalError
 
-// Throw a Custom Error Object
+// Check fro type of error
 
-function throwError() {
-    try {
-        attemptDivision();
-    } catch (error) {
-        console.log(error.message + ' - Error type: ' + error.name);
-    }
-}
-function attemptDivision() {
+function referenceError() {
     let result;
     try {
+        // Reference Error because 'x' id not defiend
         result = x / 10;
     } catch (error) {
-        // Always include at least a 'message' and 'name' properties.
-        throw {
-            'message': ' In the attemptDivision() method the following error occer: ' + error.message,
-            'name': 'CustomError'
-        };
+        handleError(error);
+    }
+}
+
+function rangeError() {
+    let result = 0;
+    try {
+        // RangeError because a number cannot have 200 significant digits 
+        result.toPrecision(200);
+    } catch (error) {
+        handleError(error);
+    }
+}
+
+function typeError() {
+    let result = 0;
+    try { // TypeError because resulyt is a numeric value
+        result.toUpperCase();
+    } catch (error) {
+        handleError(error);
+    }
+}
+
+function uriError() {
+    let uri = 'https://github.com/renuyarasu/jcl-javascript%%%syntax%%%perators';
+    try {// URI error
+        decodeURI(uri);
+    } catch (error) {
+        handleError(error);
+    }
+}
+
+function syntaxError() {
+    try {
+        // SyntaxError because missing a final single quote 
+        let sum = eval("alert('Hi there!)");
+    } catch (error) {
+        handleError(error);
+    }
+}
+
+function handleError(error) {
+    switch (error.name) {
+        case 'ReferenceError':
+            console.log('ReferenceError: ' + error.message);
+            break;
+        case 'RangeError':
+            console.log('RangeError: ' + error.message);
+            break;
+        case 'TypeError':
+            console.log('TypeError: ' + error.message);
+            break;
+        case 'URIError':
+            console.log('URIError: ' + error.message);
+            break;
+        case 'SyntaxError':
+            console.log('SyntaxError: ' + error.message);
+            break;
+        case 'EvalError':
+            console.log('EvalError: ' + error.message);
+            break;
+        default:
+            console.log('Error Type: ' + error.name + ' - Message: ' + error.message);
+            break;
     }
 }
